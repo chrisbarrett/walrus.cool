d2feff8a35e30efc4aaaaf88a717c9814f6236db
Chris Barrett <chris@walrus.cool>
2019-09-21 21:08:38 +1200

    Change the way component styling is applied

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,6 @@ import * as React from 'react';
 import { StaticQuery, graphql, Link } from 'gatsby';
 import styled from '@emotion/styled';
 
-const StyledHeader = styled.div`
-  display: flex;
-  color: gray;
-  margin: '0 auto';
-  margin-top: 1.5em;
-  margin-bottom: 2em;
-  maxwidth: 960;
-  padding: 1.45rem 1.0875rem;
-`;
-
 interface Props {
   className?: string;
 }
@@ -28,15 +18,23 @@ const Header: React.SFC<Props> = ({ className }) => (
       }
     `}
     render={data => (
-      <StyledHeader className={className}>
+      <div className={className}>
         <h1>
           <Link to="/" style={{ textDecoration: 'none', color: 'gray' }}>
             {data.site.siteMetadata.title}
           </Link>
         </h1>
-      </StyledHeader>
+      </div>
     )}
   />
 );
 
-export default Header;
+export default styled(Header)`
+  display: flex;
+  color: gray;
+  margin: '0 auto';
+  margin-top: 1.5em;
+  margin-bottom: 2em;
+  maxwidth: 960;
+  padding: 1.45rem 1.0875rem;
+`;
