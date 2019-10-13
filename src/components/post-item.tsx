956d27d8e596e7128aa15627449bcaee3788ec4d
Chris Barrett <chris@walrus.cool>
2019-10-13 16:24:52 +1300

    Update post item style

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import PostDate from './post-date';
+import styled from '@emotion/styled';
 
 export interface Props {
   id: string;
@@ -13,15 +14,32 @@ export interface Props {
   };
 }
 
+const Styles = styled.div`
+  display: flex;
+  & .post-date {
+    order: 1;
+    width: 35%;
+    text-align: right;
+    margin-top: 0.25em;
+    padding-right: 2rem;
+  }
+  & h3 {
+    vertical-align: center;
+    display: inline;
+    margin-top: 0;
+    order: 2;
+  }
+`;
+
 const PostItem: React.SFC<Props> = post => {
   const date = post.meta.date && new Date(post.meta.date);
   return (
-    <>
+    <Styles>
+      {date && <PostDate className="post-date" value={date} />}
       <h3>
         <Link to={post.fields.slug}>{post.meta.title}</Link>
       </h3>
-      {date && <PostDate value={date} />}
-    </>
+    </Styles>
   );
 };
 
